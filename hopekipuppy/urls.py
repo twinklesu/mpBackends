91175bb96a28bfa855557a8c64d68cb0a06f20f7
Subin Park <twinklesu914@gmail.com>
2020-11-21 03:41:44 +0900

    chage url

--- hopekipuppy/urls.py
+++ hopekipuppy/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url, include
 from rest_framework import routers
 from app import views
@@ -25,4 +25,5 @@ router.register(r'test', views.TestViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    url('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
