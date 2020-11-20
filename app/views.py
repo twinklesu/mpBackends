20ac77d1f708e68e45389348fe84e08d6eeb2df7
Subin Park <twinklesu914@gmail.com>
2020-11-21 02:53:56 +0900

    set rest api test

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Test
+from .serializers import TestSerializer
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+class TestViewSet(viewsets.ModelViewSet):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
--- hopekipuppy/urls.py
+++ hopekipuppy/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from testapp import views
+
+router = routers.DefaultRouter()
+router.register(r'test', views.TestViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
