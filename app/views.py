7953a2578a4df28eaa587f104ce874e545d7b4ee
Subin Park <twinklesu914@gmail.com>
2020-12-01 10:58:28 +0900

    Update views.py

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Test
 from .serializers import TestSerializer
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class TestAPIView(APIView):
     def get(self, request):
